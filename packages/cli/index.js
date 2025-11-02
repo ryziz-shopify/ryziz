60c131d3fd117abed489a0bf2a2c650d7432451d
Ryziz Team <tien.nguyen@plaqf.com>
2025-11-02 17:09:53 +0700

    chore: remove test comment

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-// CLI for Ryziz - Shopify App Framework
 import { Command } from 'commander';
 import { select } from '@inquirer/prompts';
 import { ListrInquirerPromptAdapter } from '@listr2/prompt-adapter-inquirer';
